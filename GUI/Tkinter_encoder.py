c933cd1d1b710ba699032f67422c3d9aa1b33c3a
Aarush <crce.9517.ecs@gmail.com>
2022-12-05 11:00:36 +0530

    Second commit

--- GUI/Tkinter_encoder.py
+++ GUI/Tkinter_encoder.py
@@ -1,103 +1,162 @@
 import tkinter as tk
 from tkinter import *
 import time
-# import serial
-# import threading
-# import continuous_threading
+import serial
+import threading
+import continuous_threading
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure as Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib import animation
 
-# ser = serial.Serial('COM10',9600)
-# val = 0
-# index=[]
-# ser.close()
-# ser.open()
+# Port
+ser = serial.Serial('COM10', 9600)
+val = 0
+index = []
+ser.close()
+ser.open()
 
-# def readserial():
-#     global val
-#     ser_bytes = ser.readline()
-#     ser_bytes = ser_bytes.decode("utf-8")
-#     print(ser_bytes.rstrip())
-#     val = ser_bytes
-#     index.append(val)
+# Arduino values in array
+Encoder1 = []
+Encoder2 = []
+Encoder3 = []
+Encoder4 = []
+y = []
 
-#     if len(index) == 1:
-#         display = tk.Label(root,text=index[0]).place(x=50,y=10)
+# Read data from arduino
 
-#     if len(index) == 7:
-#         index.clear()
+def readserial():
+    global val
+    ser_bytes = ser.readline()
+    ser_bytes = ser_bytes.decode("utf-8")
+    print(ser_bytes.rstrip())
+    val = ser_bytes
+    index.append(val)
 
-#     time.sleep(0.1)
+    if (len(index) == 1):
+        displayL1 = tk.Label(root, text=index[0]).place(x=70, y=70)
+    elif (len(index) == 2):
+        displayL2 = tk.Label(root, text=index[1]).place(x=70, y=200)
+    elif (len(index) == 3):
+        display3 = tk.Label(root, text=index[2]).place(x=550, y=70)
+    elif (len(index) == 4):
+        display4 = tk.Label(root, text=index[3]).place(x=550, y=200)
 
-# t1 = continuous_threading.PeriodicThread(0.1, readserial)
+    if len(index) == 4:
+
+        # Encoder1.append(index[0])
+        # Encoder2.append(index[1])
+        # Encoder3.append(index[2])
+        # Encoder4.append(index[3])
+        # anima = animation.FuncAnimation(plt.gcf(), draw_graph, interval=1500)
+        index.clear()
+
+    time.sleep(0.5)
+
+
+# Thread
+t1 = continuous_threading.PeriodicThread(0.5, readserial)
 
 root = tk.Tk()
 root.state('zoomed')
 
-# w = tk.Label(root,text="Speed:").place(x=10,y=10)
-# t1.start()
+# Button
 
 def which_button(button_press):
     print(button_press)
 
-# labelframe1 = LabelFrame(root, text="P")  
-# labelframe1.pack(fill="both", expand="yes")
-# rootlabel = Label(labelframe1, text="Contents")  
-# rootlabel.pack()  
 
-#Stop
-button = tk.Button(root, text="Stop",command=lambda m="L1 Up": which_button(m))
+# Stop
+button = tk.Button(root, text="Stop", borderwidth=10, bg="red",
+                   command=lambda m="STOP": which_button(m))
 button.place(x=250, y=10)
 
-#Battery
-B = tk.Label(root,text="Battery", font = ('calibri', 20, 'bold'),background = 'red',foreground = 'white').place(x=1000,y=0)
+# Battery
+B = tk.Label(root, text="Battery", font=('calibri', 20, 'bold'),
+             background='green', foreground='white').place(x=1000, y=0)
 
-#Left 1
-L1 = tk.Label(root,text="LF", font = ('calibri', 20, 'bold'),background = 'red',foreground = 'white').place(x=20,y=30)
+# Left 1
+L1 = tk.Label(root, text="LF", font=('calibri', 20, 'bold'),
+              background='brown', foreground='white').place(x=20, y=30)
 
-w = tk.Label(root,text="Speed:").place(x=20,y=70)
+w = tk.Label(root, text="Speed:").place(x=20, y=70)
 
-#Up button
-button = tk.Button(root, text="Up",command=lambda m="L1 Up": which_button(m))
+# Up button
+button = tk.Button(root, text="Up", command=lambda m="L1 Up": which_button(m))
 button.place(x=20, y=90)
-#Down button
-button = tk.Button(root, text="Down",command=lambda m="L1 Down": which_button(m))
+
+# Down button
+button = tk.Button(root, text="Down",
+                   command=lambda m="L1 Down": which_button(m))
 button.place(x=20, y=120)
 
-#Left 2
-L2 = tk.Label(root,text="LR", font = ('calibri', 20, 'bold'),background = 'red',foreground = 'white').place(x=20,y=160)
+# Left 2
+L2 = tk.Label(root, text="LR", font=('calibri', 20, 'bold'),
+              background='brown', foreground='white').place(x=20, y=160)
 
-w = tk.Label(root,text="Speed:").place(x=20,y=200)
+w = tk.Label(root, text="Speed:").place(x=20, y=200)
 
-#Up button
-button = tk.Button(root, text="Up",command=lambda m="L2 Up": which_button(m))
+# Up button
+button = tk.Button(root, text="Up", command=lambda m="L2 Up": which_button(m))
 button.place(x=20, y=225)
-#Down button
-button = tk.Button(root, text="Down",command=lambda m="L2 Down": which_button(m))
+
+# Down button
+button = tk.Button(root, text="Down",
+                   command=lambda m="L2 Down": which_button(m))
 button.place(x=20, y=250)
 
-#Right 1
-R1 = tk.Label(root,text="RF", font = ('calibri', 20, 'bold'),background = 'blue',foreground = 'white').place(x=500,y=30)
+# Right 1
+R1 = tk.Label(root, text="RF", font=('calibri', 20, 'bold'),
+              background='blue', foreground='white').place(x=500, y=30)
 
-w = tk.Label(root,text="Speed:").place(x=500,y=70)
+w = tk.Label(root, text="Speed:").place(x=500, y=70)
 
-#Up button
-button = tk.Button(root, text="Up",command=lambda m="R1 Up": which_button(m))
+# Up button
+button = tk.Button(root, text="Up", command=lambda m="R1 Up": which_button(m))
 button.place(x=500, y=90)
-#Down button
-button = tk.Button(root, text="Down",command=lambda m="R1 Down": which_button(m))
+
+# Down button
+button = tk.Button(root, text="Down",
+                   command=lambda m="R1 Down": which_button(m))
 button.place(x=500, y=120)
 
-#Right 2
-R2 = tk.Label(root,text="RR", font = ('calibri', 20, 'bold'),background = 'blue',foreground = 'white').place(x=500,y=160)
+# Right 2
+R2 = tk.Label(root, text="RR", font=('calibri', 20, 'bold'),
+              background='blue', foreground='white').place(x=500, y=160)
 
-w = tk.Label(root,text="Speed:").place(x=500,y=200)
+w = tk.Label(root, text="Speed:").place(x=500, y=200)
 
-#Up button
-button = tk.Button(root, text="Up",command=lambda m="R2 Up": which_button(m))
+# Up button
+button = tk.Button(root, text="Up", command=lambda m="R2 Up": which_button(m))
 button.place(x=500, y=225)
-#Down button
-button = tk.Button(root, text="Down",command=lambda m="R2 Down": which_button(m))
+
+# Down button
+button = tk.Button(root, text="Down",
+                   command=lambda m="R2 Down": which_button(m))
 button.place(x=500, y=250)
 
+# # Graph
+# count = 0
 
+
+# def draw_graph(i):
+#     global count
+#     count += 1
+#     y.append(count)
+
+#     plt.plot(Encoder1, y)
+#     plt.plot(Encoder2, y)
+#     plt.plot(Encoder3, y)
+#     plt.plot(Encoder4, y)
+
+
+# plt.xlabel("I am x")
+# plt.ylabel("I am y")
+# # anima=animation.FuncAnimation(plt.gcf(),draw_graph,interval=1500)
+# plt.grid(True)
+
+
+t1.start()
 
 root.mainloop()
